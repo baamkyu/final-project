d477b46de7b44bb12022035747c6f8441bcf5c68
kimcalm <calmszone@gmail.com>
2022-11-17 23:42:52 +0900

    1117 back - DB update

--- back-server/movies/models.py
+++ back-server/movies/models.py
@@ -1,16 +1,26 @@
 from django.db import models
 
-# TMDB 데이터
+class Genre(models.Model):
+  genre = models.CharField(max_length=20)
+
+class Nation(models.Model):
+  nation = models.CharField(max_length=20)
+
+class Director(models.Model):
+  director = models.CharField(max_length=20)
+
+class Actor(models.Model):
+  actor = models.CharField(max_length=20)
  
 class Movie(models.Model):
   movieNm = models.CharField(max_length=100)
   movieNmEn = models.CharField(max_length=100)
   showTm = models.IntegerField()
   prdtYear = models.IntegerField()
-  nations = models.TextField()
-  genres = models.TextField()
-  directors = models.TextField()
-  actors = models.TextField()
+  directors = models.ManyToManyField(Director)
+  actors = models.ManyToManyField(Actor)
+  nations = models.ManyToManyField(Nation)
+  genres = models.ManyToManyField(Genre)
 
 class Tmdb_Movie(models.Model):
   movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
@@ -20,7 +30,7 @@ class Tmdb_Movie(models.Model):
   poster_path = models.TextField(null=True)
 
 class Comment(models.Model):
-  movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+  movie = models.ForeignKey(Tmdb_Movie, on_delete=models.CASCADE)
   content = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
